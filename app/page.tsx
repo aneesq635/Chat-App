5c9069483c68490d2d8d4dc93fb925023b6fb9c8
Muhammad Anees <anees.q635@gmail.com>
2025-12-20 16:28:09 +0500

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
      <div className="min-h-screen flex flex-col  overflow-x-hidden">
-      {/* Background decoration */}
+      {/* Background decoration
       <div className="fixed top-0 left-0 w-full h-[600px] bg-gradient-to-br from-indigo-50/50 via-white to-transparent -z-10 pointer-events-none" />
       <div className="fixed -top-24 -right-24 w-96 h-96 bg-indigo-200/20 rounded-full blur-3xl -z-10" />
       
@@ -15,7 +15,7 @@ export default function Home() {
         <HeroSection />
       </main>
 
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
